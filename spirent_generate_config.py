7f9ee8bd1670d57eaf830afe71285d235d780ad4
Geoff Lawler <glawler@tislabs.com>
2018-01-16 10:00:17 -0800

    added spirent_generate_config.py which generates a traffic configuration fir Spirent. Lets set srcaddr dstaddr and protocol.

---
+++ spirent_generate_config.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3 
+
+import logging
+import json
+import argparse 
+
+from stc_session import StcSession
+from stc_config import StcConfig
+from stc_ipv4 import StcIPv4
+
+log = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(description='Create a configuration for use in other Edgelab Spirent scripts. Writes to stdout.')
+    ap.add_argument('-s', '--srcaddr', default=None, required=True, help='Source address for traffic.', type=str)
+    ap.add_argument('-d', '--dstaddr', default=None, required=True, help='Destination address for traffic.',
+                    type=str)
+    ap.add_argument('-t', '--traffic_duration', default=10, help='How long in seconds to generate traffic.',
+                    type=int)
+    ap.add_argument('-p', '--protocol', default=17, help='IP protocol to use for traffic. Default is UDP (17).',
+                    type=int)
+    args = ap.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+
+    config = StcConfig()
+    config.data[StcIPv4.config_key]['protocol'] = args.protocol
+    config.data[StcIPv4.config_key]['destAddr'] = args.dstaddr
+    config.data[StcIPv4.config_key]['sourceAddr'] = args.srcaddr
+    # hardcoded for ilab. A.B.C.D --> A.B.C.1
+    config.data[StcIPv4.config_key]['gateway'] = '.'.join(args.srcaddr.split('.')[:3]+['1'])
+    
+    print(json.dumps(config.default(), indent=4, sort_keys=True))
+
+    exit(0)
