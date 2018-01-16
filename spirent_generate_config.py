7d47436fcf336290ee23d3f2b9e464cbcab1dc13
Geoff Lawler <glawler@tislabs.com>
2018-01-16 10:03:54 -0800

    add traffic duration to config

--- spirent_generate_config.py
+++ spirent_generate_config.py
@@ -7,6 +7,7 @@ import argparse
 from stc_session import StcSession
 from stc_config import StcConfig
 from stc_ipv4 import StcIPv4
+from stc_session import StcSession
 
 log = logging.getLogger(__name__)
 
@@ -23,13 +24,19 @@ if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO)
 
-    config = StcConfig()
-    config.data[StcIPv4.config_key]['protocol'] = args.protocol
-    config.data[StcIPv4.config_key]['destAddr'] = args.dstaddr
-    config.data[StcIPv4.config_key]['sourceAddr'] = args.srcaddr
+    config = StcConfig().data
+
+    # IP config
+    config[StcIPv4.config_key]['protocol'] = args.protocol
+    config[StcIPv4.config_key]['destAddr'] = args.dstaddr
+    config[StcIPv4.config_key]['sourceAddr'] = args.srcaddr
     # hardcoded for ilab. A.B.C.D --> A.B.C.1
-    config.data[StcIPv4.config_key]['gateway'] = '.'.join(args.srcaddr.split('.')[:3]+['1'])
-    
-    print(json.dumps(config.default(), indent=4, sort_keys=True))
+    config[StcIPv4.config_key]['gateway'] = '.'.join(args.srcaddr.split('.')[:3]+['1'])
+
+    # Session config
+    config[StcSession.config_key]['traffic_duration'] = args.traffic_duration
+   
+    # write out config.
+    print(json.dumps(config, indent=4, sort_keys=True))
 
     exit(0)
